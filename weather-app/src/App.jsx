a53300c10cd769db2f677ecbd642fa2c1d54eacb
Maksym <praniknumberone@gmail.com>
2026-05-29 03:16:59 +0200

    initial commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,11 @@
+import WeatherWidget from './components/WeatherWidget';
+
+function App() {
+  return (
+    <main style={{ padding: '40px 20px' }}>
+      <WeatherWidget />
+    </main>
+  );
+}
+
+export default App;
